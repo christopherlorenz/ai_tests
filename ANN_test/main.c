e38e887dd3b4d697bb7573d3a6ef3a1be452ed2e
christopherlorenz <chris.g.lorenz@gmail.com>
2017-06-08 23:33:34 -0500

    try again

--- ANN_test/main.c
+++ ANN_test/main.c
@@ -9,35 +9,75 @@
 //
 //
 #include "create_dataset.h"
+#include "initialize_weights.h"
+#include "rescale_data.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 
 int main(int argc, char* argv[])
 {
-	int training_data_size = 2;
-	int training_data_rows = 22;
+	int dataSize = 2;
+	int dataRows = 22;
 
 	// Upper and lower bound on random values in training data (-bound is lower bound)
 	int bound = 10;
 
-	double **training_data = malloc(sizeof(double *)*training_data_rows);
-	for(int i = 0; i < training_data_rows; i++)
+	double **inputData = malloc(sizeof(double *)*dataRows);
+	double **scaledData = malloc(sizeof(double *)*dataRows);
+	for(int i = 0; i < dataRows; i++)
 	{
-		training_data[i] = malloc(sizeof(double *)*training_data_size);
+		inputData[i] = malloc(sizeof(double *)*dataSize);
+		scaledData[i] = malloc(sizeof(double *)*dataSize);
 	}
 
-	create_dataset(training_data_size, training_data_rows, training_data, bound);
+	create_dataset(dataSize, dataRows, inputData, bound);
 
-	for(int i = 0; i < training_data_rows; i++)
+	
+
+	//Read in input
+	
+	//Rescale Data
+	rescale_data(dataRows, dataSize, inputData, scaledData);
+	
+	for(int i = 0; i < dataRows; i++)
 	{
-		for(int j = 0; j < training_data_size; j++)
+		for(int j = 0; j < dataSize; j++)
 		{
-			printf("%f\t",training_data[i][j]);
+			printf("%f\t",scaledData[i][j]);
 		}
 		printf("\n");
 	}
+	
+	//Initialize Weights
+	int numInputs = 3;
+	int numOutputs = 1;	
 
+	int nNodes[3] = {numInputs, 4, numOutputs};
+	int numLayers = sizeof(nNodes)/sizeof(nNodes[0]);
+	int type = 0;
+	
+	double ***weights = malloc(sizeof(double *)*sizeof(nNodes)/sizeof(nNodes[0])-1);
+	for(int i = 0; i < sizeof(nNodes)/sizeof(nNodes[0])-1; i++)
+	{
+		weights[i] = malloc(sizeof(double *)*nNodes[i]);
+		for(int j = 0; j < nNodes[i]; j++)
+		{
+			weights[i][j] = malloc(sizeof(double *)*nNodes[i+1]);
+		}
+	}
+
+	initialize_weights(numLayers, nNodes, type, weights);
+	
+	
+	
+	//Calculate predicted values with forward propagation
+	
+	//Calculate Mean square error
+	
+	//Perform backpropagation to update values
+	
+	
 
 	return 0;
 }
